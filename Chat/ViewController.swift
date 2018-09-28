3c26b25ca516fb31a411df86192d158f9fed3a69
Kaila Prochaska <kailaprochaska@wireless-10-147-207-21.public.utexas.edu>
2018-09-27 19:51:16 -0500

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Kaila Prochaska on 9/27/18.
+//  Copyright © 2018 Kaila Prochaska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
